ceb50d55f378ada9c3d0a32eb380742a039b34fc
Zhermit09 <pavel.urvantcev@gmail.com>
2020-11-23 17:45:43 +0100

    play button

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ var vid = document.getElementById("cp2077t");
 
 function playVid() {
   vid.play();
-   document.getElementById("videoBg").style.zIndex = "-1";
+  document.getElementById("videoBg").style.zIndex = "-1";
+  document.getElementById("play").style.zIndex = "-1";
+  document.getElementById("pause").style.zIndex = "1";
 }
 
 function pauseVid() {
   vid.pause();
-   document.getElementById("videoBg").style.zIndex = "1";
+  document.getElementById("videoBg").style.zIndex = "1";
+  document.getElementById("pause").style.zIndex = "-1";
+  document.getElementById("play").style.zIndex = "1";
 }
